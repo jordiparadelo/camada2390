a594810b0ec6b48d2410fa72c4f252399c5a225c
jordiparadelo <im.jordiparadelo@gmail.com>
2017-08-30 18:28:01 -0300

    Agregue Clase1

---
+++ Clase 1/Calculadora.js
@@ -0,0 +1,35 @@
+/* Suma */
+var primerNumero = parseInt(prompt('Ingresa un numero para sumar'))
+var segundoNumero = parseInt(prompt('Ingresa el segundo numero para sumar'))
+var resultado = alert('tu resultado es ' + primerNumero + segundoNumero)
+
+/* Resta */
+var primerNumero = parseInt(prompt('Ingresa un numero para restar'))
+var segundoNumero = parseInt(prompt('Ingresa el segundo numero para restar'))
+var resultado = alert('tu resultado es ' + (primerNumero - segundoNumero))
+
+/* Multiplicacion */
+var primerNumero = parseInt(prompt('Ingresa un numero para sumar'))
+var segundoNumero = parseInt(prompt('Ingresa el segundo numero para sumar'))
+var resultado = alert('tu resultado es ' + primerNumero * segundoNumero)
+
+/* Division */
+var primerNumero = parseInt(prompt('Ingresa un numero para sumar'))
+var segundoNumero = parseInt(prompt('Ingresa el segundo numero para sumar'))
+var resultado = alert('tu resultado es ' + primerNumero / segundoNumero)
+
+
+/* Best Practices */
+
+var primerNumero = Number(prompt('Ingresa un numero para sumar'))
+var segundoNumero = Number(prompt('Ingresa el segundo numero para sumar'))
+
+var suma = primerNumero + segundoNumero
+var resta = primerNumero - segundoNumero
+var multiplicar = primerNumero * segundoNumero
+var dividir = primerNumero / segundoNumero
+
+alert(suma)
+alert(resta)
+alert(multiplicar)
+alert(dividir)
